af5d4bcb447d0e1f6004ccec94159942b5803f87
Roar1986 <roar.boyum@gmail.com>
2024-03-25 12:28:09 +0100

    fire første metoder ok

--- smarthouse/api.py
+++ smarthouse/api.py
@@ -1,10 +1,12 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from smarthouse.persistence import SmartHouseRepository
 from pathlib import Path
+from typing import List,Dict,Union
 import os
+
 def setup_database():
     project_dir = Path(__file__).parent.parent
     db_file = project_dir / "data" / "db.sql" # you have to adjust this if you have changed the file name of the database
@@ -59,26 +61,76 @@ def get_smarthouse_info() -> dict[str, int | float]:
 
 # Få informasjon frå alle etasjar
 @app.get("/smarthouse/floor")
-def get_smarthouse_floor() -> dict[str,int | float]:
-    return{
-       "no_floors": len(smarthouse.get_floors()),
-        
-    }
+def get_smarthouse_floor() -> List[Dict[str, Union[int | List]]]:
+    floorInfo = smarthouse.get_floors()
+    floorsList = []  # Lager ei tom liste
+    for floor in floorInfo: # Går gjennom alle etasjer
+        floorData = {
+            "floor level": floor.level,
+        }
+        floorsList.append(floorData)  # Legger til etasje i ei liste
+    return floorsList  # Returnerer ei liste som inneheld alle etasjer
+
 
 # Få informasjon om ein gitt etasje gitt av "fid" = FloorID
 @app.get("/smarthouse/floor/{fid}")
-def get_smarthouse_floor_specific() -> dict[str,int | float]:
-    pass
-
+def get_smarthouse_floor_specific(fid: int) -> dict[str,int]:
+    
+    floorInfo = smarthouse.get_floors()
+    floorData = "", None
+    for floors in floorInfo:
+        if floors.level == fid:
+            return {
+                "Floor Level": floors.level
+            }
+         
+    raise HTTPException(status_code=404, detail="No given floor with this id was found")
+         
+    
 # Få informasjon om alle room på ein gitt etasje "fid" = FloorID
 @app.get("/smarthouse/floor/{fid}/room")
-def get_smarthouse_AllroomsAtSpecificFloor() -> dict[str,int | float]:
-    pass
+def get_smarthouse_AllroomsAtSpecificFloor(fid : int) -> List[Dict[str, int | str | float]]:
+    
+    # Henter alle rom
+    getAllRooms = smarthouse.get_rooms()
+    # Oppretter ei tom liste
+    roomList = []
+
+    # Sjekker om etasjen eksisterer
+    checkFloor = get_smarthouse_floor_specific(fid)
+
+    if checkFloor:
+        for rooms in getAllRooms:
+            if int(rooms.floor.level) == int(fid):
+                roomData = {
+                    "Floor Level" : rooms.floor.level,
+                    "room name" : rooms.room_name,
+                    "room size" : rooms.room_size
+                }
+                roomList.append(roomData)
+        return roomList
+
 
 # Informasjon om ein spesific rom {rid} "RoomID" på ein gitt etasje {fid} "FloorID" 
 @app.get("/smarthouse/floor/{fid}/room/{rid}")
-def get_smarthouse_roomAtSpecificFloor()-> dict[str,int | float]:
-    pass
+def get_smarthouse_roomAtSpecificFloor(fid : int, rid : str)-> List[Dict[str, int | str | float]]:
+
+    # Sjekker om etasjen eksisterer
+    HenterData = get_smarthouse_floor_specific(fid)
+    getAllRooms = smarthouse.get_rooms()
+    roomList = []
+
+    if HenterData:
+        for rooms in getAllRooms:
+            if str(rooms.rooms_name) == str(rid):
+                roomData = {
+                    "Floor Level" : rooms.floor.level,
+                    "room name" : rooms.room_name,
+                    "room size" : rooms.room_size
+                }
+                roomList.append(roomData)
+        return roomList
+
 
 # --------- Det skal finnes endepunkter for tilgang til enheter -------------------------------------
 
@@ -95,34 +147,34 @@ def get_smarthouse_device()-> dict[str,int | float]:
 # --------- Det skal finnes spesielle endepunkter for tilgang til sensor funksjoner -----------------
 
 # Get current sensor måling for sensor "uuid" = DeviceID 
-@app.get("smarthouse/sensor/{uuid}/current")
+@app.get("/smarthouse/sensor/{uuid}/current")
 def get_smarthouse_sensor_currentMeasurment()-> dict[str,int | float]:
     pass
 
 # Legg til måling for sensor "uuid" = DeviceID 
-@app.post("smarthouse/sensor/{uuid}/current")
+@app.post("/smarthouse/sensor/{uuid}/current")
 def post_smarthouse_sensor_Measurment()-> dict[str,int | float]:
     pass
 
 #  get n siste målinger for sensor uuid. om query parameter ikkje er tilgjengelig, den alle tilgjengelege målinger.
-@app.get("GET smarthouse/sensor/{uuid}/values?limit=n")
+@app.get("/smarthouse/sensor/{uuid}/values?limit=n")
 def get_smarthouse_sensor_MeasurmentLatestAvailable()-> dict[str,int | float]:
     pass
 
 # Slett gamleste måling for sensor uuid
-@app.delete("smarthouse/sensor/{uuid}/oldest")
+@app.delete("/smarthouse/sensor/{uuid}/oldest")
 def delete_smarthouse_sensor_MeasurmentLatestAvailable()-> dict[str,int | float]:
     pass
 
 # --------- Det skal finnes spesielle endepunkter for tilgang til aktuator funskjoner ---------------
 
 # get current state for actuator uuid
-@app.get("smarthouse/actuator/{uuid}/current")
+@app.get("/smarthouse/actuator/{uuid}/current")
 def get_smarthouse_actuatorCurrentState()-> dict[str,int | float]:
     pass
 
 # oppdater current state for actuator uuid
-@app.put("smarthouse/device/{uuid}")
+@app.put("/smarthouse/device/{uuid}")
 def put_smarthouse_actuatorCurrentState()-> dict[str,int | float]:
     pass
 
